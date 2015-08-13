29b29dcf5287f9087a8d7f220a9886a8d700b6c7
Keenan Brock <keenan@thebrocks.net>
2015-08-12 22:07:35 -0400

    pipeline support for neato

--- lib/html/pipeline/dot_filter.rb
+++ lib/html/pipeline/dot_filter.rb
@@ -4,18 +4,17 @@ require 'dothtml/dot_helper'
 module HTML
   class Pipeline
     class DotFilter < Filter
-      LANGUAGES=%w(dot).freeze
+      LANGUAGES=%w(dot neato).freeze
 
       def call
         doc.search("pre").each do |node|
           code = node.children.first
           lang = must_str(code && code["class"])
           next unless LANGUAGES.include?(lang)
-          text = code.inner_text
+          doc = DotHelper.from_dot(code.inner_text, lang)
+          doc.remove_comments
 
-          dom = DotHelper.from_dot(text, lang).fix_ids.remove_comments.node
-
-          node.replace(dom).first
+          node.replace(doc.node).first
         end
 
         doc
